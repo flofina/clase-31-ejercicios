c4bc790baa4bfe61b881e8f4de406ccab88536ff
flofina <mf.guzzi@gmail.com>
2019-10-18 21:56:38 -0300

    ejercicios operadores js

---
+++ ejercicios.js
@@ -0,0 +1,137 @@
+/* ej. 27 */
+
+console.log("ej. 27");
+
+let numero1 = 2;
+
+let numero2 = 3;
+
+let resultadoSuma = numero1 + numero2;
+
+let resultadoResta = numero1 - numero2;
+
+let resultadoMulti = numero1 * numero2;
+
+let resultadoDivision = numero1 / numero2;
+
+let resultadoResto = numero1 % numero2;
+
+console.log("sumo " + numero1 + " + " + numero2 + " y el resultado es " + resultadoSuma);
+
+console.log("resto " + numero1 + " - " + numero2 + " y el resultado es " + resultadoResta);
+
+console.log("multiplico " + numero1 + " - " + numero2 + " y el resultado es " + resultadoMulti);
+
+console.log("divido " + numero1 + " - " + numero2 + " y el resultado es " + resultadoDivision);
+
+console.log("el resto de las variables " + numero1 + " y " + numero2 + " es " + resultadoResto);
+
+/* ej. 28 */
+
+console.log("ej. 28");
+
+let numero = 9;
+
+let resultado1 = numero * 1;
+let resultado2 = numero * 2;
+let resultado3 = numero * 3;
+let resultado4 = numero * 4;
+let resultado5 = numero * 5;
+let resultado6 = numero * 6;
+let resultado7 = numero * 7;
+let resultado8 = numero * 8;
+let resultado9 = numero * 9;
+let resultado10 = numero * 10;
+
+
+console.log(numero + " x 1 = " + resultado1);
+
+console.log(numero + " x 2 = " + resultado2);
+
+console.log(numero + " x 3 = " + resultado3);
+
+console.log(numero + " x 4 = " + resultado4);
+
+console.log(numero + " x 5 = " + resultado5);
+
+console.log(numero + " x 6 = " + resultado6);
+
+console.log(numero + " x 7 = " + resultado7);
+
+console.log(numero + " x 8 = " + resultado8);
+
+console.log(numero + " x 9 = " + resultado9);
+
+console.log(numero + " x 10 = " + resultado10);
+
+/* ej. 29 */
+
+console.log("ej. 29");
+
+let lado = 10;
+
+let resultadoPerimetro = lado * 4;
+
+console.log("el perimetro de un cuadrado de " + lado + "cm de lado es " + resultadoPerimetro + "cm");
+
+/* ej. 30 */
+
+console.log("ej. 30");
+
+let resultadoArea = lado * lado;
+
+console.log("el area de un cuadrado de " + lado + "cm de lado es " + resultadoArea + "cm");
+
+/* ej. 31 */
+
+console.log("ej. 31");
+
+let lado1 = 10;
+let lado2 = 20;
+let lado3 = 5;
+
+let perimetroTriangulo = lado1 + lado2 + lado3;
+
+console.log("el perimetro de un triangulo es " + perimetroTriangulo + "cm");
+
+/* ej. 32 */
+
+console.log("ej. 32");
+
+let altura = 10;
+let base = 4;
+
+let areaTriangulo = base * altura;
+
+console.log("el area de un triangulo es " + areaTriangulo + "cm");
+
+/* ej. 33 */
+
+console.log("ej. 33");
+
+let cantidadDePersonas = 100;
+
+cantidadDePersonas += 5;
+
+console.log("cantidad de personas: " + cantidadDePersonas);
+
+cantidadDePersonas -= 3;
+
+console.log("cantidad de personas: " + cantidadDePersonas);
+
+cantidadDePersonas *= 2;
+
+console.log("cantidad de personas: " + cantidadDePersonas);
+
+/* ej. 35 */
+
+console.log("ej. 35");
+
+let numeroDeCliente = 143245;
+
+let numeroDeClienteIngresado = "143245";
+
+console.log("el numero ingresado por el cliente es correcto: ", numeroDeCliente == numeroDeClienteIngresado);
+
+console.log("el numero ingresado por el cliente es correcto: ", numeroDeCliente === numeroDeClienteIngresado);
+
